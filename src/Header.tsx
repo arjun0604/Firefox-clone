d4e2aba809a325e1db7ad867d4fccdba81672493
Arjun Santhosh <arjunsanthosh11b2@gmail.com>
2026-08-12 16:00:30 +0530

    fix: Fixed css issues

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,18 +6,20 @@ function Header(){
         <>  
             <img 
                 src={firefoxLogo} 
-                className="bg-center bg-no-repeat w-[120px] h-[56px] m-0"
+                className="bg-center bg-no-repeat w-[120px] h-[56px] m-5"
             />
-            <div className="relative flex justify-center">
-                <img 
-                    src={googleLogo} 
-                    className="absolute h-[35px] w-[35px] z-10 top-1/2 -translate-y-1/2 left-[calc(50%-338px+10px)]"
-                />
-                <input 
-                    className="m-[10px] w-[675px] rounded-[18px] bg-white py-[11px] pr-[11px] pl-[44px] border-none outline-none text-black" 
-                    type="text" 
-                    placeholder="Search with Google or enter address"
-                />
+            <div className="flex justify-center mt-4">
+                <div className="relative w-[675px]">
+                    <img 
+                        src={googleLogo} 
+                        className="absolute h-[30px] w-[30px] left-3 top-1/2 -translate-y-1/2 pointer-events-none"
+                    />
+                    <input 
+                        className="w-full rounded-[18px] bg-white py-[11px] pr-[11px] pl-[48px] border-none outline-none text-black shadow-sm" 
+                        type="text" 
+                        placeholder="Search with Google or enter address"
+                    />
+                </div>
             </div>
         </>
     );
