b18702fef5f897f038bbf998bab436bd3432e83e
Arjun Santhosh <arjunsanthosh11b2@gmail.com>
2026-08-12 17:37:27 +0530

    feat: Shortcut feature added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import Header from "./Header"
+import Shortcut from "./Shortcut"
 
 function App() {
 
   return (
     <div className="bg-[#210340] text-white min-h-screen">
       <Header/>
+      <div className="flex column justify-center">
+      <Shortcut name="Youtube" 
+                url="https://www.youtube.com/"
+                img="src/assets/shortcuts/youtube_logo.avif"
+      />
+
+      <Shortcut name="ChatGPT"
+                url="https://chatgpt.com/"
+                img="src/assets/shortcuts/chatgpt_logo.png"
+      />
+
+      <Shortcut name="Gmail"
+                url="https://workspace.google.com/intl/en-US/gmail/"
+                img="src/assets/shortcuts/gmail_logo.webp"
+      />
+
+      <Shortcut name="LinkedIn"
+                url="https://www.linkedin.com/"
+                img="src/assets/shortcuts/linkedin_logo.png"
+      />
+
+      <Shortcut name="Netflix"
+                url="https://www.netflix.com/"
+                img="src/assets/shortcuts/netflix_logo.png"
+      />
+
+      <Shortcut name="Amazon"
+                url="https://www.amazon.com/"
+                img="src/assets/shortcuts/amazon_logo.webp"
+      />
+
+      </div>
+      
     </div>
   )
 }
---
+++ src/Shortcut.tsx
@@ -0,0 +1,30 @@
+interface ShortcutProps {
+    name: string;
+    url: string;
+    img: string;
+}
+
+function Shortcut({ name, url, img }: ShortcutProps) {
+    return (
+        <div className="flex flex-col items-center">
+            <a
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-16 h-16 rounded-full overflow-hidden flex items-center justify-center mt-10 mr-4 ml-4 shadow-lg hover:shadow-2xl hover:scale-120 transition-all duration-200 border-0"
+            >
+                <img
+                    src={img}
+                    alt={name}
+                    className="w-full h-full object-cover"
+                />
+            </a>
+
+            <p className="text-white text-sm mt-2 font-medium">
+                {name}
+            </p>
+        </div>
+    );
+}
+
+export default Shortcut;
