ce90122c92ad80886ff0ca4273589dcfd770ca37
Arjun Santhosh <arjunsanthosh11b2@gmail.com>
2026-08-13 18:49:14 +0530

    feat: Background change setup done

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,16 +6,16 @@ function Header(){
         <>  
             <img 
                 src={firefoxLogo} 
-                className="bg-center bg-no-repeat w-[120px] h-[56px] m-5"
+                className="bg-center bg-no-repeat w-32 h-14 m-5"
             />
             <div className="flex justify-center mt-4">
-                <div className="relative w-[675px]">
+                <div className="relative w-full max-w-2xl px-4">
                     <img 
                         src={googleLogo} 
-                        className="absolute h-[30px] w-[30px] left-3 top-1/2 -translate-y-1/2 pointer-events-none"
+                        className="absolute h-8 w-8 left-7 top-1/2 -translate-y-1/2 pointer-events-none"
                     />
                     <input 
-                        className="w-full rounded-[18px] bg-[#2B2A3A] py-[11px] pr-[11px] pl-[48px] border-none outline-none text-white shadow-sm" 
+                        className="w-full rounded-2xl bg-[#2B2A3A] py-3 pr-3 pl-14 border-none outline-none text-white shadow-sm" 
                         type="text" 
                         placeholder="Search with Google or enter address"
                     />
